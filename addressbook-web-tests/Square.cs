8a485f68d142d2db5ab498d342ac4e4d51c33a43
Ислямов Марсель Равилович <Marsel.Islyamov@norbit.ru>
2021-03-21 18:14:40 +0300

    Создан 1 класс

---
+++ addressbook-web-tests/Square.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace addressbook_web_tests
+{
+    class Square
+    {
+        //если не назначить размер, по умолчанию будет size = 0;
+        // или можно так:
+        //private int fSize;
+        //
+        //public Square(int size)
+        //{
+        //    fSize = size;
+        //}
+        
+        private int size;
+        
+        public Square(int size)
+        {
+            this.size = size;
+        }
+
+        public int Size
+        {
+            get
+            {
+                return size;
+            }
+            set
+            {
+                size = value;
+            }
+        //Или можно так2
+        //}
+        //public int getSize()
+        //{
+        //    return fSize;
+        //}
+        //
+        //public void setSize(int size) {
+        //    fSize = size;
+        //}
+    }
+}
--- addressbook-web-tests/UnitTest1.cs
+++ addressbook-web-tests/UnitTest1.cs
@@ -9,6 +9,27 @@ namespace addressbook_web_tests
         [TestMethod]
         public void TestMethod1()
         {
+            Square s1 = new Square(5);
+            Square s2 = new Square(10);
+            Square s3 = s1;
+
+            Assert.AreEqual(s1.Size, 5);
+            Assert.AreEqual(s2.Size, 10);
+            Assert.AreEqual(s3.Size, 5);
+
+            s3.Size = 15;
+
+            Assert.AreEqual(s1.Size, 15);
         }
-    }
+
+        // или можно так
+            //Assert.AreEqual(s1.getSize(), 5);
+            //Assert.AreEqual(s2.getSize(), 10);
+            //Assert.AreEqual(s3.getSize(), 5);
+            //
+            //s3.setSize(15);
+            //
+            //Assert.AreEqual(s1.getSize(), 15);
+        }
+}
 }
