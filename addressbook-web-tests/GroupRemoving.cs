8796ea329df29e9fe742665e64dbf97234c8cf9a
Ислямов Марсель Равилович <Marsel.Islyamov@norbit.ru>
2021-04-05 21:13:18 +0300

    Содание Теста на удаление Группы Создание Общего класса для тестов

---
+++ addressbook-web-tests/GroupRemoving.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using NUnit.Framework;
+
+namespace addressbook_web_tests
+{
+    [TestFixture]
+    class GroupRemovingTests : TestBase
+    { 
+
+        [Test]
+        public void GroupRemovingTest()
+        {
+            GoToHomePage();
+            Login(new AccountData("admin","secret"));
+            NewMethod();
+            SelectGroup(1);
+            RemoveGroup();
+            ReturnToGroupsPage();
+        }
+    }
+}
